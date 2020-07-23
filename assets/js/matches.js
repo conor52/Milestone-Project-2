7cbd6ba22a7e0f41080435b1fc2d49593305c91f
conor52 <conormccarthy157@gmail.com>
2020-07-23 10:00:27 +0000

    tweaked table styling, fixed matches.js switch statement

--- assets/js/matches.js
+++ assets/js/matches.js
@@ -17,54 +17,50 @@ function getData(match, cb) {
 }
 
 function writeToDocument(match) {
-    var match = '';
-    var game_number='';
+    var match;
+    var game_number;
     switch(match) {
         case 1:
-            match = 560862;
+            match =560862;
             game_number=1;
             break;
         case 2:
-            match = 560863;
+            match =560863;
             game_number=2;
             break;
         case 3:
-            match = 560864;
+            match =560864;
             game_number=3;
             break;
         case 4:
-            match = 560865;
+            match =560865;
             game_number=4;
             break;
         case 5:
-            match = 560866;
+            match =560866;
             game_number=5;
             break;
     }
-
     getData(match, function(data) {
         var gameDetails = [];
         var el = document.getElementById("game-"+game_number);
         data.forEach(function(item) {
             var matchRow = [];
-            matchRow.push('<p class="matchup-style">' + item['name'] + '</p>');
+            matchRow.push('<p class="matchup-style white match-table-font">' + item['name'] + '</p>');
             gameDetails.push(`<tr>${matchRow}</tr>`);
         });
         el.innerHTML = `<p>${gameDetails}</p>`;
     });
-
     getData(match, function(data) {
-        var gameDetails = []
+        var gameDetails = [];
         var el = document.getElementById("league-and-format-"+game_number);
         data.forEach(function(item) {
             var matchRow = [];
-           matchRow.push('<p class="league-and-format-style">' + item['league']['name'] + '<br>Best of 1</p>');
+           matchRow.push('<p class="white match-table-font">' + item['league']['name'] + '<br>Best of 1</p>');
             gameDetails.push(`<tr>${matchRow}</tr>`);
         });
-        
-        console.log(gameDetails.length)
-        if(gameDetails.length > 0){
+       if(gameDetails.length > 0){
             el.innerHTML = `<p>${gameDetails}</p>`;
-        }
+      }
     });
 }
